db7da1ce2101f8b5d7ff5842032eaa53a092b12b
Małgorzata Żak <80418203+Ra1lgunz@users.noreply.github.com>
2021-03-16 13:32:34 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,h;
+    float V;
+
+    std::cin>>a>>h;
+    V=(a*a*h)/3;
+    std::cout << V << std::endl;
+
+    return 0;
+}
