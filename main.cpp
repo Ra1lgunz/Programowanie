c023d5aba239ccc01d5c2b87557109c9ed4cf485
Małgorzata Żak <80418203+Ra1lgunz@users.noreply.github.com>
2021-03-16 13:36:15 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+
+int main()
+{
+    int x;
+    std::cin>>x;
+
+    if(x>0)
+    {
+        std::cout<<"Liczba jest dodatnia";
+    }
+    else if (x<0)
+    {
+        std::cout<<"Liczba jest ujemna";
+    }
+    else
+    {
+        std::cout<<"Podana liczba to 0";
+    }
+    return 0;
+}
